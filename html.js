78e2d436b77378612a15e534cb870823827b1775
Odayaka <35795454+odayakanaasa@users.noreply.github.com>
2018-04-08 16:32:01 +0700

    Update html.js

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-IDHERE',
+    google_ad_client: 'ca-pub-3229806070275118',
     enable_page_level_ads: true
   });
   `;
